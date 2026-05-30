89cb99cce3e90c4102f236f54426adc16c7c6579
whitecloudy <alex9395@naver.com>
2026-05-30 13:46:20 +0900

    let generate_rf use general inference sampler

--- generate_rf.py
+++ generate_rf.py
@@ -21,51 +21,53 @@ import joblib
 from huggingface_hub import hf_hub_download
 import json
 from training.sampler import padding_mask_from_original_shape
+from training.sampler import inference_edm_sampler as edm_sampler
+
 #----------------------------------------------------------------------------
 # Proposed EDM sampler (Algorithm 2).
 
-def edm_sampler(
-    net, latents, class_labels=None, randn_like=torch.randn_like,
-    num_steps=18, sigma_min=0.002, sigma_max=80, rho=7,
-    S_churn=0, S_min=0, S_max=float('inf'), S_noise=1,
-    stop_variance=0.0, padding_mask = 1
-):
-    # Adjust noise levels based on what's supported by the network.
-    sigma_min = max(sigma_min, net.sigma_min)
-    sigma_max = min(sigma_max, net.sigma_max)
+# def edm_sampler(
+#     net, latents, class_labels=None, randn_like=torch.randn_like,
+#     num_steps=18, sigma_min=0.002, sigma_max=80, rho=7,
+#     S_churn=0, S_min=0, S_max=float('inf'), S_noise=1,
+#     stop_variance=0.0, padding_mask = 1
+# ):
+#     # Adjust noise levels based on what's supported by the network.
+#     sigma_min = max(sigma_min, net.sigma_min)
+#     sigma_max = min(sigma_max, net.sigma_max)
 
-    # Time step discretization.
-    step_indices = torch.arange(num_steps, dtype=torch.float64, device=latents.device)
-    t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
-    t_steps = torch.cat([net.round_sigma(t_steps), torch.zeros_like(t_steps[:1])]) # t_N = 0
+#     # Time step discretization.
+#     step_indices = torch.arange(num_steps, dtype=torch.float64, device=latents.device)
+#     t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
+#     t_steps = torch.cat([net.round_sigma(t_steps), torch.zeros_like(t_steps[:1])]) # t_N = 0
 
-    # Main sampling loop.
-    x_next = latents.to(torch.float64) * t_steps[0] * padding_mask
-    for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])): # 0, ..., N-1
-        x_cur = x_next
+#     # Main sampling loop.
+#     x_next = latents.to(torch.float64) * t_steps[0] * padding_mask
+#     for i, (t_cur, t_next) in enumerate(zip(t_steps[:-1], t_steps[1:])): # 0, ..., N-1
+#         x_cur = x_next
 
-        # Increase noise temporarily.
-        gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
-        t_hat = net.round_sigma(t_cur + gamma * t_cur)
-        x_hat = (x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * randn_like(x_cur)) * padding_mask
+#         # Increase noise temporarily.
+#         gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
+#         t_hat = net.round_sigma(t_cur + gamma * t_cur)
+#         x_hat = (x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * randn_like(x_cur)) * padding_mask
 
-        # Euler step.
-        denoised = net(x_hat, t_hat, class_labels).to(torch.float64) * padding_mask
+#         # Euler step.
+#         denoised = net(x_hat, t_hat, class_labels).to(torch.float64) * padding_mask
         
-        # Stop if variance is below threshold
-        if t_next ** 2 < stop_variance:
-            return denoised
+#         # Stop if variance is below threshold
+#         if t_next ** 2 < stop_variance:
+#             return denoised
 
-        d_cur = (x_hat - denoised) / t_hat
-        x_next = x_hat + (t_next - t_hat) * d_cur
+#         d_cur = (x_hat - denoised) / t_hat
+#         x_next = x_hat + (t_next - t_hat) * d_cur
 
-        # Apply 2nd order correction.
-        if i < num_steps - 1:
-            denoised = net(x_next, t_next, class_labels).to(torch.float64) * padding_mask
-            d_prime = (x_next - denoised) / t_next
-            x_next = x_hat + (t_next - t_hat) * (0.5 * d_cur + 0.5 * d_prime)
+#         # Apply 2nd order correction.
+#         if i < num_steps - 1:
+#             denoised = net(x_next, t_next, class_labels).to(torch.float64) * padding_mask
+#             d_prime = (x_next - denoised) / t_next
+#             x_next = x_hat + (t_next - t_hat) * (0.5 * d_cur + 0.5 * d_prime)
 
-    return x_next
+#     return x_next
 
 #----------------------------------------------------------------------------
 # Generalized ablation sampler, representing the superset of all sampling
